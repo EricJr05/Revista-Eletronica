7f1a071280ef909ab8cf6820df3dc14ff25386d3
EricJr05 <eric.j.oliveira@aluno.senai.br>
2025-03-05 14:13:18 -0300

    Delete Revista_Eletronica/protect.php

--- Revista_Eletronica/protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(!isset($_SESSION)){
-    session_start();
-}
-
-if(!isset($_SESSION['id']) || $_SESSION['nivel'] < 3){
-    die('Você não pode acessar essa página pois não está logado. <a href="index.php">Entrar</a>');
-}
